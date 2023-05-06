54fcb0d3e7617cd841d3f614c71ab3a6d1e20bda
Ferlony <Hegecioum@gmail.com>
2023-05-06 16:28:33 +0500

    init

---
+++ themes/powerarrow-dark/wallpapers.lua
@@ -0,0 +1,10 @@
+local function wallpapers()
+    local wallpaper = {}
+    wallpaper.DSsoller                      = "wall.png"
+    wallpaper.DiscoElysium                  = "dep.png"
+
+    local chosen_wallpaper = wallpaper.DiscoElysium
+    
+    return chosen_wallpaper
+end
+return wallpapers
